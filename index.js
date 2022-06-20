00c9cde08887c7007214c92459d27535718c04b5
Lottie Rugg <lottie.ruggeasey@gmail.com>
2022-06-20 16:48:41 +0100

    routes get and post

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = 3000
 app.use(express.json())
 app.use(cors())
 const url= 'mongodb://root:password@localhost:27017'
+app.use(express.urlencoded({extended: true}))
 
 app.get('/events/:id', async (req, res) => {
     const userId = ObjectId(req.params.id)
@@ -16,10 +17,25 @@ app.get('/events/:id', async (req, res) => {
     const db = connection.db('gifti')
     const collection = db.collection('events')
     const data = await collection.findOne({_id:userId})
-
     res.json(data)
 })
 
+app.post('/events', async (req, res) =>{
+    const dataToInsert = {
+        event_name: req.body.event_name,
+        deadline: req.body.deadline,
+        participants: []
+    }
+    console.log(req.body)
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('gifti')
+    const collection = db.collection('events')
+    const result = await collection.insertOne(dataToInsert)
+    res.json(dataToInsert)
+})
 
-
+app.get('*', (req, res)=>{
+    res.status(404)
+    res.send('Page not found!')
+})
 app.listen(port)
