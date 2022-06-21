d034588998875f2f273f0bcc724c6498eac0074b
Seb Lamberts <seb.lamberts@eyup.com>
2022-06-21 14:12:52 +0100

    made API RESTful

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const app = express()
 const port = 3000
 const url= 'mongodb://root:password@localhost:27017'
 
+/**
+ * Returns a connection to the 'gifti' database
+ *
+ * @returns {Promise<Db>}
+ */
 const getDb = async () => {
     let connection = {}
     connection = await MongoClient.connect(url, {ignoreUndefined: true})
     return connection.db('gifti')
 }
 
+/**
+ * Middleware to connect to the events collection and store it in res.locals. Should used as a route specific callback
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<*>}
+ */
 const dbMiddleware = async (req, res, next) => {
     let connection = null
     try {
@@ -31,9 +44,61 @@ const dbMiddleware = async (req, res, next) => {
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
-app.use(dbMiddleware)
 
-app.get('/events/:id', async (req, res) => {
+// Below are the API routes, grouped by route and ordered GET>POST>PUT>DELETE
+
+// /events route
+
+app.get('/events', async (req, res) =>{
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
+})
+
+app.post('/events', dbMiddleware, async (req, res) =>{
+    const dataToInsert = {
+        event_name: req.body.event_name,
+        deadline: req.body.deadline,
+        participants: []
+    }
+
+    const result = await res.locals.collection.insertOne(dataToInsert)
+    if(result.insertedId) {
+        res.status(200).json({
+            status: 200,
+            message: 'Document added successfully',
+            data: {id: result.insertedId}
+        })
+    } else {
+        res.status(400).json({
+            status: 400,
+            message: 'Failed to add document',
+            data: null
+        })
+    }
+})
+
+app.put('/events', async (req, res) =>{
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
+})
+
+app.delete('/events', async (req, res) =>{
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
+})
+
+// /events/:id Route
+
+app.get('/events/:id', dbMiddleware, async (req, res) => {
     let _id
     try {
         _id = ObjectId(req.params.id)
@@ -61,33 +126,36 @@ app.get('/events/:id', async (req, res) => {
     }
 })
 
-app.post('/events', async (req, res) =>{
-    const dataToInsert = {
-        event_name: req.body.event_name,
-        deadline: req.body.deadline,
-        participants: []
-    }
-
-    const result = await res.locals.collection.insertOne(dataToInsert)
-    if(result.insertedId) {
-        res.status(200).json({
-            status: 200,
-            message: 'Document added successfully',
-            data: {id: result.insertedId}
-        })
-    } else {
-        res.status(400).json({
-            status: 400,
-            message: 'Failed to add document',
-            data: null
-        })
-    }
+app.post('/events/:id', async (req, res) => {
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
 })
 
-app.get('*', (req, res)=>{
+app.put('/events/:id', async (req, res) => {
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
+})
+
+app.delete('/events/:id', async (req, res) => {
+    res.status(405).json({
+        "status": 405,
+        "message": "Method not allowed",
+        "data": null
+    })
+})
+
+// Catch-all route
+
+app.all('*', (req, res) =>{
     res.status(404).json({
         status: 404,
-        message: 'Page not found!',
+        message: 'Route not implemented, see documentation at: https://github.com/iO-Academy/2022-mar-gifti-api',
         data: null
     })
 })
